b695c3f7e38b1507db431eceb9579e0a015cbe2c
karl368 <lincon368@126.com>
2014-11-21 17:52:54 +0800

    add form
    add a form

--- VsQuickTest/Program.cs
+++ VsQuickTest/Program.cs
@@ -11,6 +11,28 @@ namespace VsQuickTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, c#!");
+            helloWord();
+            testLinqStatement();
+        }
+        
+        private static void helloWord() {
+            System.Console.WriteLine("Hello, C#!");
+        }
+
+        private static void testLinqStatement()
+        {
+            System.Console.WriteLine("----------");
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var lowNums =
+                 from n in numbers
+                 where n < 5
+                 select n;
+            Console.WriteLine("Numbers < 5:");
+            foreach (var x in lowNums)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
