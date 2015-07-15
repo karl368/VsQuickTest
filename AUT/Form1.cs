36b26715d591593fc636dafe29ecb59429cb90c3
karl368 <lincon368@126.com>
2015-07-15 08:47:01 +0800

    try reflection test

---
+++ AUT/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VsQuickTest.AUT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tb = textBox2.Text;
+            string cb = comboBox1.Text;
+
+            if (tb == cb)
+                listBox1.Items.Add("Even");
+            else if (tb == "paper" && cb == "rock" ||
+                tb == "rock" && cb == "scissor" ||
+                tb == "scissor" && cb == "paper")
+                listBox1.Items.Add("win");
+            else
+                listBox1.Items.Add("loss");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ AutTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Launch Form...");
+            Form theForm = null;
+            string formName = "VsQuickTest.AUT.Form1";
+            string path = "";
+
+        }
+    }
+}
